187cefea3f70c5d00bf7d70c9c1bcefbb5644791
Shahaji.Kokane <Shahaji.Kokane@riomed.com>
2025-02-05 22:56:20 +0530

    second commit

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -3,6 +3,7 @@ const YOUTUBE_API_VIDEOS = `https://youtube.googleapis.com/youtube/v3/videos?par
 
 export default YOUTUBE_API_VIDEOS;
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
+  // 'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
 export const LIVE_CHAT_COUNT = 25;
